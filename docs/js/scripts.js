c046a416b80d68aec31bb975a8e3f20504e6e2ea
Christian López Carretero <christianlc00@gmail.com>
2021-06-17 09:18:02 +0200

    Estructura inicial

---
+++ docs/js/scripts.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     let elems = document.querySelectorAll('.scrollspy');
+//     let instances = M.ScrollSpy.init(elems, options);
+// });
+
+// $(document).ready(function () {
+//     $('.scrollspy').scrollSpy();
+// });
